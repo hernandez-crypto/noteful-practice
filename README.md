f3d5bfd50ec9af734d4cc781c282389410403ad7
julio hernandez <hernandezjulio58@ymail.com>
2019-11-11 13:47:17 -0800

    added blogful

---
+++ README.md
@@ -0,0 +1,2 @@
+seeding into db : psql -U (user) -d (db) -f (file path)
+ex: psql -U dunder_mifflin -d blogful -f ./seeds/seed.blogful_articles.sql
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/blogful',
+};
