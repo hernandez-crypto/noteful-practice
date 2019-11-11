5c9ec09d1eb803e9145ceb23dee45c52674b37e6
julio hernandez <hernandezjulio58@ymail.com>
2019-11-11 15:12:42 -0800

    migration scripts

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const express = require('express');
-const xss = require('xss');
-const ArticlesService = require('./articles-service');
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const ArticlesService = require("./articles-service");
 
-const articlesRouter = express.Router();
+const foldersRouter = express.Router();
 const jsonParser = express.json();
 
-const serializeArticle = (article) => ({
+const serializeFolder = article => ({
   id: article.id,
   style: article.style,
   title: xss(article.title),
   content: xss(article.content),
   date_published: article.date_published,
-  author: article.author,
+  author: article.author
 });
 
-articlesRouter
-  .route('/')
+foldersRouter
+  .route("/")
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db');
+    const knexInstance = req.app.get("db");
     ArticlesService.getAllArticles(knexInstance)
-      .then((articles) => {
-        res.json(articles.map(serializeArticle));
+      .then(articles => {
+        res.json(articles.map(serializeFolder));
       })
       .catch(next);
   })
@@ -32,27 +32,27 @@ articlesRouter
     for (const [key, value] of Object.entries(newArticle))
       if (value == null)
         return res.status(400).json({
-          error: { message: `Missing '${key}' in request body` },
+          error: { message: `Missing '${key}' in request body` }
         });
 
-    ArticlesService.insertArticle(req.app.get('db'), newArticle)
-      .then((article) => {
+    ArticlesService.insertArticle(req.app.get("db"), newArticle)
+      .then(article => {
         res
           .status(201)
           .location(path.posix.join(req.originalUrl, `/${article.id}`))
-          .json(serializeArticle(article));
+          .json(serializeFolder(article));
       })
       .catch(next);
   });
 
-articlesRouter
-  .route('/:article_id')
+foldersRouter
+  .route("/:article_id")
   .all((req, res, next) => {
-    ArticlesService.getById(req.app.get('db'), req.params.article_id)
-      .then((article) => {
+    ArticlesService.getById(req.app.get("db"), req.params.article_id)
+      .then(article => {
         if (!article) {
           return res.status(404).json({
-            error: { message: `Article doesn't exist` },
+            error: { message: "Article doesn't exist" }
           });
         }
         res.article = article;
@@ -61,11 +61,11 @@ articlesRouter
       .catch(next);
   })
   .get((req, res, next) => {
-    res.json(serializeArticle(res.article));
+    res.json(serializeFolder(res.article));
   })
   .delete((req, res, next) => {
-    ArticlesService.deleteArticle(req.app.get('db'), req.params.article_id)
-      .then((numRowsAffected) => {
+    ArticlesService.deleteArticle(req.app.get("db"), req.params.article_id)
+      .then(numRowsAffected => {
         res.status(204).end();
       })
       .catch(next);
@@ -79,19 +79,20 @@ articlesRouter
     if (numberOfValues === 0)
       return res.status(400).json({
         error: {
-          message: `Request body must contain either 'title', 'style' or 'content'`,
-        },
+          message:
+            "Request body must contain either 'title', 'style' or 'content'"
+        }
       });
 
     ArticlesService.updateArticle(
-      req.app.get('db'),
+      req.app.get("db"),
       req.params.article_id,
-      articleToUpdate,
+      articleToUpdate
     )
-      .then((numRowsAffected) => {
+      .then(numRowsAffected => {
         res.status(204).end();
       })
       .catch(next);
   });
 
-module.exports = articlesRouter;
+module.exports = foldersRouter;
