e27dbff995cddf8ad326284e3dce31cd4495cf33
julio hernandez <hernandezjulio58@ymail.com>
2019-11-12 09:27:21 -0800

    api working

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const articlesRouter = require('./articles/articles-router');
-const usersRouter = require('./users/users-router');
-const commentsRouter = require('./comments/comments-router');
+const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
 
 const app = express();
 
@@ -16,9 +15,12 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use('/api/articles', articlesRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/comments', commentsRouter);
+app.use('/api/folders', foldersRouter);
+app.use('/api/notes', notesRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
