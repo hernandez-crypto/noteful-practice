fe9117a60634fceabd0727b3c2dfe7415dd62da0
julio hernandez <hernandezjulio58@ymail.com>
2019-12-01 11:43:01 -0800

    deploymenty

--- src/app.js
+++ src/app.js
@@ -22,10 +22,6 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.get('/api', (req, res) => {
-  res.send('Almost there!!');
-});
-
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
